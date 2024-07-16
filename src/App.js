d08f697b3860df545d02cfc4133eaf9e55f1c44c
app1 <saieesh.soma1919@gmail.com>
2024-07-16 11:23:30 +0530

    Git

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ import Footer from "./components/footer/Footer.js";
 import Orders from "./components/order/Orders.js";
 import Register from "./components/user/Register.js";
 
-
 function App() {
-  const PATH = process.env.REACT_APP_PATH;
+  const PATH = process.env.REACT_APP_PATH || "/sneakers";
 
   return (
     <div className="App">
-      <Router>
-           <Navbar />
-        <hr></hr>
+      <Router basename={PATH}>
+        <Navbar />
+        <hr />
         <Routes>
-          <Route path={`${PATH}/`} element={<Products />} />
-          <Route path={`${PATH}/order`} element={<Orders />} />
-          <Route path={`${PATH}/cart`} element={<Cart />} />
-          <Route path={`${PATH}/login`} element={<Login />} />
-          <Route path={`${PATH}/register`} element={<Register />} />
+          <Route path="/" element={<Products />} />
+          <Route path="/order" element={<Orders />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
         <Footer />
       </Router>
     </div>
   );
 }
+
 export default App;
