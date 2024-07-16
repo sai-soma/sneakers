40bca0777f22d02f914b7a277fe6f8409bae631e
app1 <saieesh.soma1919@gmail.com>
2024-07-16 10:57:02 +0530

    first commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Footer from "./Footer.css";
+export default function () {
+  return (
+    <>
+      <div className="Footer-container">
+        <div>
+          <h3>About us</h3>
+          SneakerHood<br></br>
+          Our Company<br></br>
+          Our Brands<br></br>
+        </div>
+        <div>
+          <h3>Quick Links</h3>
+          Privacy Policy<br></br>
+          Delivery<br></br>
+          Season's Discounts<br></br>
+        </div>
+        <div>
+          <h3>Our Responsibility</h3>
+          Community<br></br>
+          Latest Styles<br></br>
+          Premium Quality<br></br>
+        </div>
+        <div>
+          <h3>Social Media</h3>
+          Facebook<br></br>
+          Twitter<br></br>
+          Instagram
+        </div>
+      </div>
+         <div className="Footer-bottom">
+        <div>Web Accessiblity | Privacy Statement | Terms of Use Contact Us</div>
+        <div>© 2024
+        SneakerHood Company. @All rights reserved.</div> 
+      </div>
+    </>
+  );
+}
